5c7efe24061626a2ba7a5c475c08603d30a96059
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2021-01-01 17:25:25 +1100

    g

---
+++ Assets/Scripts/Generator/AvailableRooms.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvailableRooms : MonoBehaviour
+{
+    public GameObject[] rooms;
+    public static int roomsHere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Generator/RoomSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public AvailableRooms roomavailable;
+    public GameObject[] neighbour;
+    public Collider[] zone;
+    private bool HasRoom;
+    private int random;
+    private float timer = 1;
+    
+    private void Awake()
+    {
+        roomavailable = FindObjectOfType<AvailableRooms>();
+        AvailableRooms.roomsHere += 1;
+
+    }
+    private void Start()
+    {
+        roomavailable = FindObjectOfType<AvailableRooms>();
+        for (int i = 0; i < neighbour.Length; i++)
+        {
+            zone = Physics.OverlapSphere(neighbour[i].transform.position, 2f);
+            HasRoom = false;
+            for (int j = 0; j < zone.Length; j++)
+            {
+                if (zone[j].tag == "Spawn")
+                {
+                    HasRoom = true;
+
+                }
+            }
+            if (HasRoom != true && AvailableRooms.roomsHere <= 200)
+            {
+                random = Random.Range(0, roomavailable.rooms.Length);
+                Instantiate(roomavailable.rooms[random], neighbour[i].transform.position, Quaternion.identity);
+                
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
